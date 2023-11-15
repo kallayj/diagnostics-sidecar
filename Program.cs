834aadef4ead31118f5cc9993b6ce66f5b5cb188
Jonathan Kallay <jonathan.kallay@cariad.us>
2023-11-15 13:45:55 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using HelloAKS;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
